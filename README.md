9a75c373d5fc221d2c22b78e57641a6a6c70e39d
Andrew Kroh <andy@crowbird.com>
2014-02-23 09:18:57 -0500

    Adding template for RHEL 6.5 server.

--- README.md
+++ README.md
@@ -2,3 +2,33 @@ veewee-rhel-templates
 =====================
 
 Veewee Templates for Creating Redhat Vagrant Boxes
+
+Requirements
+------------
+VirtualBox 4.3.6
+Vagrant 1.4.3
+Ruby 1.9.3-p484
+DevKit-tdm-32-4.5.2-20111229-1559-sfx.exe (extract to /c/Ruby193/DevKit)
+Git Bash
+# Configure PATH to include ruby, vagrant, VBoxManage
+
+# Using Git Bash as terminal
+cd /c/Ruby193/DevKit
+ruby dk.rb init
+ruby dk.rb.install
+
+# Install veewee from source:
+git clone https://github.com/jedi4ever/veewee.git
+cd veewee
+gem install bundler
+bundle install
+alias veewee='bundle exec veewee'
+
+# Download the Veewee RHEL6 template:
+cd ..
+git clone https://github.com/jmassara/veewee-rhel6-vbox.git
+cp -pR ../veewee-rhel6-vbox/rhel-server-6.4-x86_64-minimal templates
+veewee vbox define 'rhel64-minimal' 'rhel-server-6.4-x86_64-minimal'
+veewee vbox build 'rhel64-minimal'
+veewee vbox export 'rhel64-minimal'
+
---
+++ rhel-server-6.5-x86_64-minimal/definition.rb
@@ -0,0 +1,43 @@
+Veewee::Session.declare({
+  :cpu_count => '1',
+  :memory_size=> '512',
+  :disk_size => '10240',
+  :disk_format => 'VDI',
+  :hostiocache => 'off',
+  :virtualbox => {
+    :vm_options => [
+        'pae' => 'on',
+        'ioapic' => 'on'
+    ]
+  },
+  :os_type_id => 'RedHat6_64',
+  :iso_file => "rhel-server-6.5-beta-x86_64-dvd.iso",
+  :iso_src => "http://your-server.local/rhel-server-6.5-beta-x86_64-dvd.iso",
+  :iso_md5 => "c95ba10cba6bd21b76e6cb6ccdf95ac8",
+  :iso_download_timeout => 1000,
+  :boot_wait => "15",
+  :boot_cmd_sequence => [
+    '<Tab> text ks=http://%IP%:%PORT%/ks.cfg<Enter>'
+  ],
+  :kickstart_port => "7122",
+  :kickstart_timeout => 10000,
+  :kickstart_file => "ks.cfg",
+  :ssh_login_timeout => "10000",
+  :ssh_user => "vagrant",
+  :ssh_password => "vagrant",
+  :ssh_key => "",
+  :ssh_host_port => "7222",
+  :ssh_guest_port => "22",
+  :sudo_cmd => "echo '%p'|sudo -S sh '%f'",
+  :shutdown_cmd => "/sbin/halt -h -p",
+  :postinstall_files => [
+    "base.sh",
+    "chef.sh",
+    "puppet.sh",
+    "vagrant.sh",
+    "virtualbox.sh",
+    "cleanup.sh",
+    "zerodisk.sh"
+  ],
+  :postinstall_timeout => 10000
+})
